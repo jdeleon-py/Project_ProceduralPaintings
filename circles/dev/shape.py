53fccc341fd65200d978d7e54ff338123759087b
JamesdeLeon <jdeleonphys@gmail.com>
2021-11-02 21:42:36 -0500

    add structure to account for PIL methods

--- circles/dev/shape.py
+++ circles/dev/shape.py
@@ -10,9 +10,15 @@ class Shape:
 	ATTRIBUTES:
 		-
 	'''
-	def __init__(self):
-		pass
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
 
+		self.transparency = random.randint(0, 255)
+		self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), self.transparency)
+
+	def draw_circle(self, draw):
+		draw.ellipse((x0, y0, x1, y1), self.color)
 
 if __name__ == "__main__":
 	pass
